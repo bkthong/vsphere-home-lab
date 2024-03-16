62453c6541c38bd98ac66aeb4a5a2d4747bfcc38
bk <jase.bkthong@gmail.com>
2024-03-16 14:46:42 +0800

    Updated create-esx11.sh to have second disk for use as vmfs data store

--- create-esx11.sh
+++ create-esx11.sh
@@ -6,6 +6,8 @@
 # Allocating 16GB of ram as vcenter would require 14GB as vm appliance
 # Would need to give more storage for data store as a separate disk (TODO)
 # To also have separate nics for clustering and etc (FUTURE todo)
+#
+# Ref: https://docs.vmware.com/en/VMware-vSphere/8.0/vsphere-esxi-801-installation-setup-guide.pdf
 virt-install \
               --name esx11 \
               --memory 16384 \
@@ -13,6 +15,7 @@ virt-install \
               --cpu host \
               --boot uefi \
               --machine q35 \
+              --disk size=20,target.bus=sata \
               --disk size=50,target.bus=sata \
               --cdrom /isos/VMware-VMvisor-Installer-8.0U2-22380479.x86_64.iso \
               --network network=default,model=e1000e \
